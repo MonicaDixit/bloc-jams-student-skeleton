e4b1dafcab620595d51b00a6c1315132990d2230
Monica <mdixit81@gmail.com>
2015-01-19 11:33:19 -0800

    checkpoint 30

--- public/javascripts/album.js
+++ public/javascripts/album.js
@@ -30,18 +30,42 @@ var albumPicasso = {
      ]
  };
 
+ var currentlyPlayingSong = null;
+
  var createSongRow = function(songNumber, songName, songLength){
    var template = 
    '<tr>'
-   +' <td class = "col-md-1">' + songNumber + '</td>'
+   +' <td class = "song-number" "col-md-1" data-song-number = " ' +songNumber+ ' ">' + songNumber + '</td>'
    + '<td class = "col-md-9">' + songName +  '</td>'
    +  '<td class = "colx-md-2">' + songLength + '</td>'
    + '</tr>'
    ;
 
-   return $(template); 
+   var $row = $(template);
+ 
+   var onHover = function(event) {
+     var songNumberCell = $(this).find('.song-number');
+     var songNumber = songNumberCell.data('song-number');
+     if (songNumberCell !== currentlyPlayingSong){
+        songNumberCell.html('<a class = "album-song-button"><i class = "fa fa-play"></i></a>');
+     }
+     songNumberCell.html('<a class="album-song-button"><i class="fa fa-play"></i></a>');
+   };
+ 
+   var offHover = function(event) {
+     var songNumberCell = $(this).find('.song-number');
+     var songNumber = songNumberCell.data('song-number');
+     if(songNumber !== currentlyPlayingSong){
+        songNumberCell.html(songNumber);
+     }
+   };
+ 
+   $row.find(' .song-number').click(clickHandler);
+   $row.hover(onHover, offHover);
+   return $row;
 
  };
+
 
  var changeAlbumView = function (album){
    var $albumTitle = $(' .album-title');
@@ -63,6 +87,26 @@ var albumPicasso = {
       var songData = songs[i];
       var $newRow = createSongRow(i + 1, songData.name, songData.length );
       $songList.append($newRow);
+   }
+
+ };
+
+ var clickHandler = function(event){
+   var songNumber = $(this).data('song-number');
+   if(currentlyPlayingSong != null){
+     var currentlyPlayingCell = $(' .song-number[data-song-number="' + currentlyPlayingSong + '"]');
+     currentlyPlayingCell.html(currentlyPlayingSong);
+   }
+
+   if (currentlyPlayingSong !== songNumber){
+     $(this).html('<a class = "album-song-button"><i class = "fa fa-pause"></i></a>');
+     currentlyPlayingSong = songNumber;
+   }
+
+   else if (currentlyPlayingSong === songNumber){
+     $(this).html('<a class = "album-song-button"><i class = "fa fa-play"></i></a>');
+     currentlyPlayingSong = null;
+
    }
 
  };
