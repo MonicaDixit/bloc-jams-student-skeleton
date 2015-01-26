aa488f9198c62e69d450c33e2cabe59db871f803
Monica <mdixit81@gmail.com>
2015-01-25 21:43:10 -0800

    checkpoint 468

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -18,3 +18,22 @@ $(document).ready(function(){
   $('.selling-points .point').hover(onHoverAction, offHoverAction);
 });
 ;
+
+angular.module('BlocJams', []).controller('Landing.controller',['$scope', function($scope){
+  $scope.subText =  "Turn down for what !" ;
+  $scope.subTextClicked = function(){
+    $scope.subText += '!' ;
+  };
+
+  $scope.albumURLs = [
+     '/images/album-placeholders/album-1.jpg',
+     '/images/album-placeholders/album-2.jpg',
+     '/images/album-placeholders/album-3.jpg',
+     '/images/album-placeholders/album-4.jpg',
+     '/images/album-placeholders/album-5.jpg',
+     '/images/album-placeholders/album-6.jpg',
+     '/images/album-placeholders/album-7.jpg',
+     '/images/album-placeholders/album-8.jpg',
+     '/images/album-placeholders/album-9.jpg',
+  ];
+}]);
