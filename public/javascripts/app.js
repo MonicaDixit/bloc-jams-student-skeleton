3cea1bcaa981e610bb5d8ba53877107ff4996db0
Monica <mdixit81@gmail.com>
2015-02-09 00:27:37 -0800

    Added volume control: checkpoint 47

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -115,6 +115,14 @@
   blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
     $scope.songPlayer = SongPlayer;
 
+    $scope.volumeClass = function() {
+      return {
+        'fa-volume-off': SongPlayer.volume == 0,
+        'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+        'fa-volume-up': SongPlayer.volume > 70
+      }
+    }
+
     SongPlayer.onTimeUpdate(function(event, time){
       $scope.$apply(function(){
         $scope.playTime = time;
@@ -131,6 +139,7 @@
       currentSong: null,
       currentAlbum: null,
       playing: false,
+      volume: 90,
  
       play: function() {
         this.playing = true;
@@ -171,6 +180,12 @@
       onTimeUpdate: function(callback) {
         return $rootScope.$on('sound:timeupdate', callback);
       },
+      setVolume: function(volume) {
+        if(currentSoundFile){
+          currentSoundFile.setVolume(volume);
+        }
+        this.volume = volume;
+      },
       setSong: function(album, song) {
         if (currentSoundFile) {
           currentSoundFile.stop();
@@ -182,6 +197,7 @@
         formats: [ "mp3" ],
         preload: true
       });
+      currentSoundFile.setVolume(this.volume);
 
       currentSoundFile.bind('timeupdate', function(e){
         $rootScope.$broadcast('sound:timeupdate', this.getTime());
